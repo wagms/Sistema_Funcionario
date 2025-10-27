33f33b416edf68d8e80e834244962af5ea5103b1
wagms <132621029+wagms@users.noreply.github.com>
2025-10-27 20:20:37 -0300

    Delete Frontend - Pedro Assumpção/Sistema_Funcionarios directory

--- Frontend - Pedro Assumpção/Sistema_Funcionarios/src/auth.guard.ts
+++
@@ -1,9 +0,0 @@
-import { inject } from '@angular/core';
-import { Router, UrlTree } from '@angular/router';
-import { AuthService } from './services/auth.service';
-
-export function authGuard(): boolean | UrlTree {
-  const auth = inject(AuthService);
-  const router = inject(Router);
-  return auth.isLoggedIn() ? true : router.parseUrl('/login');
-}
